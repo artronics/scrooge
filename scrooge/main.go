4178233737a2c1afc1d6495c97d88546eef59291
artronics <jalalhosseiny@gmail.com>
2023-03-04 00:38:09 +0000

    add dynamo example query

--- scrooge/main.go
+++ scrooge/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 )
 
 type MyEvent struct {
@@ -16,5 +18,20 @@ func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
 
 func main() {
 	fmt.Println("running!")
+	cfg, err := awsConf()
+	if err != nil {
+		panic(err.Error())
+	}
+	table := NewResourceTable(cfg, "scrooge_resources")
+	table.GetActive()
 	lambda.Start(HandleRequest)
+}
+
+func awsConf() (*aws.Config, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-2"))
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
